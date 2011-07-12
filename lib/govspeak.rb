4117770acc24b9b4ab07fd54172f3206573fba52
Ben Griffiths <bengriffiths@gmail.com>
2011-07-12 11:58:33 +0100

    Ok, so we do this the regexp way.

--- lib/govspeak.rb
+++ lib/govspeak.rb
@@ -2,9 +2,40 @@ require 'kramdown'
 
 module Govspeak
   
-  class Document < Kramdown::Document
+  class Document 
+    
+    @@extensions = []
+    
+    def initialize(source,options = {})
+      @doc = Kramdown::Document.new(preprocess(source),options)
+      super
+    end  
+  
+    def to_html
+      @doc.to_html
+    end
+  
+    def preprocess(source)
+      @@extensions.each do |title,block|
+        regexp = %r${::#{title}}(.*?){:/#{title}}$m
+        source.gsub!(regexp) {|match|
+          block.call($1)
+        }
+      end
+      source
+    end
+    
+    def self.extension(title,&block)
+      @@extensions << [title,block]
+    end
+    
+    extension('reverse') { |body|
+      body.reverse
+    }
     
   end
   
-end
   
+  
+end
+
--- test/govspeak_test.rb
+++ test/govspeak_test.rb
@@ -7,4 +7,10 @@ class GovspeakTest < Test::Unit::TestCase
     assert_equal "<p><em>this is markdown</em></p>\n", rendered
   end
   
+  test "simple block extension" do
+    rendered =  Govspeak::Document.new("this \n{::reverse}\n*is*\n{:/reverse}\n markdown").to_html
+    assert_equal "<p>this </p>\n\n<p><em>si</em></p>\n\n<p>markdown</p>\n", rendered
+  end
+  
+  
 end
