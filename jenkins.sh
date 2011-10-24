def91cf861340725d0ae023b92e389a472bdf14c
Paul Battley <pbattley@gmail.com>
2011-10-24 15:30:58 +0100

    Remove RVM from build script.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -x
-source '/usr/local/lib/rvm'
-export GEM_HOME="/home/jenkins/bundles/${JOB_NAME}"
-mkdir -p "${GEM_HOME}"
-bundle install && bundle exec rake
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake
