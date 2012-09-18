da55f87c962593744b2ff33ad3bce34f04f97939
John Griffin <johnog@gmail.com>
2012-09-18 16:15:02 +0100

    Revert "Fix encoding and spacing issues in html validator"
    This reverts commit 46a3c0ee416877bb0409b40563bdd05b91e160b1.

--- lib/govspeak/html_validator.rb
+++ lib/govspeak/html_validator.rb
@@ -1,6 +1,3 @@
-require "equivalent-xml"
-require 'htmlentities'
-
 class Govspeak::HtmlValidator
   attr_reader :string
 
@@ -15,15 +12,15 @@ class Govspeak::HtmlValidator
   def valid?
     dirty_html = govspeak_to_html
     clean_html = Govspeak::HtmlSanitizer.new(dirty_html).sanitize
-    EquivalentXml.equivalent?(normalize_entities(dirty_html),
-                              normalize_entities(clean_html))
+    normalise_html(dirty_html) == normalise_html(clean_html)
+  end
+
+  # Make whitespace in html tags consistent
+  def normalise_html(html)
+    Nokogiri::HTML.parse(html).to_s
   end
 
   def govspeak_to_html
     Govspeak::Document.new(string).to_html
-  end
-
-  def normalize_entities(html)
-    HTMLEntities.new.decode(html)
   end
 end
