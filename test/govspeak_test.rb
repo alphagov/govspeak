50e9a9c975cde5c436875c5ae289714b9c18a70f
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-09-26 13:49:21 +0100

    Move link tests to separate file

--- test/govspeak_test.rb
+++ test/govspeak_test.rb
@@ -905,43 +905,4 @@ Or so we thought.}
     rendered = Govspeak::Document.new(govspeak).to_html
     assert_match("", rendered)
   end
-
-  test "embedded link with link provided" do
-    link = {
-      content_id: "5572fee5-f38f-4641-8ffa-64fed9230ad4",
-      url: "https://www.example.com/",
-      title: "Example website"
-    }
-    govspeak = "[embed:link:5572fee5-f38f-4641-8ffa-64fed9230ad4]"
-    rendered = Govspeak::Document.new(govspeak, {links: link}).to_html
-    expected = %r{<a href="https://www.example.com/">Example website</a>}
-    assert_match(expected, rendered)
-  end
-
-  test "embedded link with markdown title" do
-    link = {
-      content_id: "5572fee5-f38f-4641-8ffa-64fed9230ad4",
-      url: "https://www.example.com/",
-      title: "**Example website**"
-    }
-    govspeak = "[embed:link:5572fee5-f38f-4641-8ffa-64fed9230ad4]"
-    rendered = Govspeak::Document.new(govspeak, {links: link}).to_html
-    expected = %r{<a href="https://www.example.com/"><strong>Example website</strong></a>}
-    assert_match(expected, rendered)
-  end
-  test "embedded link with link not provided" do
-    link = {
-      content_id: "e510f1c1-4862-4333-889c-8d3acd443fbf",
-      title: "Example website",
-    }
-    govspeak = "[embed:link:e510f1c1-4862-4333-889c-8d3acd443fbf]"
-    rendered = Govspeak::Document.new(govspeak, {links: link}).to_html
-    assert_match("Example website", rendered)
-  end
-
-  test "embedded link without link object" do
-    govspeak = "[embed:link:0726637c-8c66-47ad-834a-d815cbf51e0e]"
-    rendered = Govspeak::Document.new(govspeak).to_html
-    assert_match("", rendered)
-  end
 end
