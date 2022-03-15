9d3214633d1d623cd27fe7545c8b0f2bf3694de7
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-03-15 12:38:48 +0000

    Don't validate changes made by post-processors
    The HTML Validator was sanitizing Markdown which had been converted to HTML and had post-processors applied. This meant that any unsafe HTML added by a post-processor was causing the source Markdown to be considered invalid.
    
    Logically, it seems more sensible for HTML to be validated after conversion from Markdown, but **before** the post-processors have altered the HTML.
    
    This way, we only validate user input rather than validating the output of our own post-processors, which we consider to be trusted.

--- lib/govspeak/html_validator.rb
+++ lib/govspeak/html_validator.rb
@@ -11,17 +11,19 @@ class Govspeak::HtmlValidator
   end
 
   def valid?
-    dirty_html = govspeak_to_html
-    clean_html = Govspeak::HtmlSanitizer.new(dirty_html, @sanitization_options).sanitize
+    dirty_html = govspeak_to_html(sanitize: false)
+    clean_html = govspeak_to_html(sanitize: true)
     normalise_html(dirty_html) == normalise_html(clean_html)
   end
+
+private
 
   # Make whitespace in html tags consistent
   def normalise_html(html)
     Nokogiri::HTML5.fragment(html).to_s
   end
 
-  def govspeak_to_html
-    Govspeak::Document.new(govspeak_string, sanitize: false).to_html
+  def govspeak_to_html(sanitize:)
+    Govspeak::Document.new(govspeak_string, sanitize: sanitize).to_html
   end
 end
