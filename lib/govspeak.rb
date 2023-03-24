1a3e8b7a4fb062b0c4cd07111a3379903667af5c
Murilo Dal Ri <murilodalri@gmail.com>
2023-03-24 12:17:28 +0000

    bundle exec rubocop -A
    ```
    bundle exec rubocop -A
    Inspecting 41 files
    ...C.....................................
    
    Offenses:
    
    lib/govspeak.rb:35:18: C: [Corrected] Style/RedundantFreeze: Do not freeze immutable objects, as freezing them has no effect.
        UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.freeze
                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ```
    41 files inspected, 1 offense detected, 1 offense corrected

--- lib/govspeak.rb
+++ lib/govspeak.rb
@@ -32,7 +32,7 @@ module Govspeak
   class Document
     Parser = Kramdown::Parser::Govuk
     PARSER_CLASS_NAME = Parser.name.split("::").last
-    UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.freeze
+    UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
     NEW_PARAGRAPH_LOOKBEHIND = %q{(?<=\A|\n\n|\r\n\r\n)}.freeze
 
     @extensions = []
