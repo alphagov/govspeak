c2907605481c29a35103a2bd564a1baba86f6a23
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2024-11-19 15:32:35 +0000

    WIP Remove copy-pasted Kramdown code from override
    We were revisiting Joe's old CTA and legislative list bug fixes in
    mobbing today. We wanted to know whether we could remove the code that
    we'd had to copy from the Kramdown codebase. The results turned out to
    look spookily similar to the code that Joe had originally removed
    (see: https://github.com/alphagov/govspeak/pull/298)

--- lib/kramdown/parser/govuk.rb
+++ lib/kramdown/parser/govuk.rb
@@ -56,108 +56,21 @@ module Kramdown
         super
       end
 
-      # We override the parse_list method with a modified version where ordered lists are
-      # disabled (for use with legislative lists). The majority of the method body is copied
-      # over (from https://github.com/gettalong/kramdown/blob/REL_2_3_1/lib/kramdown/parser/kramdown/list.rb#L54),
-      # only the LIST_START is changed. Previously, we dynamically modified some of the
-      # class-scoped variables used in this method; this provides a thread-safe alternative.
+      # Override Kramdown's native `parse_list` to avoid parsing OLs (including
+      # nested ones) when the `ordered_lists_disabled` flag is set on a block.
       def parse_list
         return super unless @options[:ordered_lists_disabled]
 
         return if @src.check(LIST_START_OL)
 
-        start_line_number = @src.current_line_number
-        type, list_start_re = (@src.check(LIST_START_UL) ? [:ul, LIST_START_UL] : [:ol, LIST_START_OL])
-        list = new_block_el(type, nil, nil, location: start_line_number)
+        original_list_start = LIST_START
+        Kramdown::Parser::Kramdown.send(:remove_const, :LIST_START)
+        Kramdown::Parser::Kramdown.send(:const_set, :LIST_START, LIST_START_UL)
 
-        item = nil
-        content_re, lazy_re, indent_re = nil
-        eob_found = false
-        nested_list_found = false
-        last_is_blank = false
-        until @src.eos?
-          start_line_number = @src.current_line_number
-          if last_is_blank && @src.check(HR_START)
-            break
-          elsif @src.scan(EOB_MARKER)
-            eob_found = true
-            break
-          elsif @src.scan(list_start_re)
-            list.options[:first_list_marker] ||= @src[1].strip
-            item = Element.new(:li, nil, nil, location: start_line_number)
-            item.value, indentation, content_re, lazy_re, indent_re =
-              parse_first_list_line(@src[1].length, @src[2])
-            list.children << item
-
-            item.value.sub!(self.class::LIST_ITEM_IAL) do
-              parse_attribute_list(::Regexp.last_match(1), item.options[:ial] ||= {})
-              ""
-            end
-
-            list_start_re = fetch_pattern(type, indentation)
-            nested_list_found = (item.value =~ LIST_START_UL)
-            last_is_blank = false
-            item.value = [item.value]
-          elsif (result = @src.scan(content_re)) || (!last_is_blank && (result = @src.scan(lazy_re)))
-            result.sub!(/^(\t+)/) { " " * 4 * ::Regexp.last_match(1).length }
-            indentation_found = result.sub!(indent_re, "")
-            if !nested_list_found && indentation_found && result =~ LIST_START_UL
-              item.value << +""
-              nested_list_found = true
-            elsif nested_list_found && !indentation_found && result =~ LIST_START_UL
-              result = " " * (indentation + 4) << result
-            end
-            item.value.last << result
-            last_is_blank = false
-          elsif (result = @src.scan(BLANK_LINE))
-            nested_list_found = true
-            last_is_blank = true
-            item.value.last << result
-          else
-            break
-          end
-        end
-
-        @tree.children << list
-
-        last = nil
-        list.children.each do |it|
-          temp = Element.new(:temp, nil, nil, location: it.options[:location])
-
-          env = save_env
-          location = it.options[:location]
-          it.value.each do |val|
-            @src = ::Kramdown::Utils::StringScanner.new(val, location)
-            parse_blocks(temp)
-            location = @src.current_line_number
-          end
-          restore_env(env)
-
-          it.children = temp.children
-          it.value = nil
-
-          it_children = it.children
-          next if it_children.empty?
-
-          # Handle the case where an EOB marker is inserted by a block IAL for the first paragraph
-          it_children.delete_at(1) if it_children.first.type == :p &&
-            it_children.length >= 2 && it_children[1].type == :eob && it_children.first.options[:ial]
-
-          if it_children.first.type == :p &&
-              (it_children.length < 2 || it_children[1].type != :blank ||
-                (it == list.children.last && it_children.length == 2 && !eob_found)) &&
-              (list.children.last != it || list.children.size == 1 ||
-                list.children[0..-2].any? { |cit| !cit.children.first || cit.children.first.type != :p || cit.children.first.options[:transparent] })
-            it_children.first.children.first.value << "\n" if it_children.size > 1 && it_children[1].type != :blank
-            it_children.first.options[:transparent] = true
-          end
-
-          last = (it_children.last.type == :blank ? it_children.pop : nil)
-        end
-
-        @tree.children << last if !last.nil? && !eob_found
-
-        true
+        super
+      ensure
+        Kramdown::Parser::Kramdown.send(:remove_const, :LIST_START)
+        Kramdown::Parser::Kramdown.send(:const_set, :LIST_START, original_list_start)
       end
     end
   end
