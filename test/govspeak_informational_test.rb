e1642076e3a534a3f216b6a65515ea1170982152
Ynda Jas <yndajas@gmail.com>
2025-06-16 18:21:58 +0100

    Use clearer test descriptions for informational

--- test/govspeak_informational_test.rb
+++ test/govspeak_informational_test.rb
@@ -4,29 +4,35 @@ require "govspeak_test_helper"
 class GovspeakImagesTest < Minitest::Test
   include GovspeakTestHelper
 
-  test_given_govspeak("^ I am very informational ^") do
-    assert_html_output %(
-      <div role="note" aria-label="Information" class="application-notice info-notice">
-      <p>I am very informational</p>
-      </div>)
-    assert_text_output "I am very informational"
+  test "renders okay with just an opening tag" do
+    given_govspeak "^ I am very informational" do
+      assert_html_output %(
+        <div role="note" aria-label="Information" class="application-notice info-notice">
+        <p>I am very informational</p>
+        </div>)
+      assert_text_output "I am very informational"
+    end
   end
 
-  test_given_govspeak "The following is very informational\n^ I am very informational ^" do
-    assert_html_output %(
-      <p>The following is very informational</p>
-
-      <div role="note" aria-label="Information" class="application-notice info-notice">
-      <p>I am very informational</p>
-      </div>)
-    assert_text_output "The following is very informational I am very informational"
+  test "renders okay with opening and closing tags" do
+    given_govspeak("^ I am very informational ^") do
+      assert_html_output %(
+        <div role="note" aria-label="Information" class="application-notice info-notice">
+        <p>I am very informational</p>
+        </div>)
+      assert_text_output "I am very informational"
+    end
   end
 
-  test_given_govspeak "^ I am very informational" do
-    assert_html_output %(
-      <div role="note" aria-label="Information" class="application-notice info-notice">
-      <p>I am very informational</p>
-      </div>)
-    assert_text_output "I am very informational"
+  test "avoids combining into a single block with an immediately-preceding line" do
+    given_govspeak "The following is very informational\n^ I am very informational ^" do
+      assert_html_output %(
+        <p>The following is very informational</p>
+
+        <div role="note" aria-label="Information" class="application-notice info-notice">
+        <p>I am very informational</p>
+        </div>)
+      assert_text_output "The following is very informational I am very informational"
+    end
   end
 end
