d6bc4cfa4300705f50ee331dabace060d4f5d1d3
Ynda Jas <yndajas@gmail.com>
2025-06-18 03:37:48 +0100

    Improve informational test coverage
    I found usage of these when searching Whitehall for informational markup
    
    An example with all of these features is:
    https://www.gov.uk/guidance/heat-health-alerting-system-guidance-for-voluntary-and-community-sectors
    
    I'm extending `Minitest::Spec::DSL` here to allow usage of `describe`
    blocks. We haven't used these elsewhere in Govspeak tests, but I think
    it makes specs easier to make sense of and navigate

--- test/govspeak_informational_test.rb
+++ test/govspeak_informational_test.rb
@@ -3,6 +3,7 @@ require "govspeak_test_helper"
 
 class GovspeakImagesTest < Minitest::Test
   include GovspeakTestHelper
+  extend Minitest::Spec::DSL
 
   test "renders okay with just an opening tag" do
     given_govspeak "^ I am very informational" do
@@ -24,6 +25,16 @@ class GovspeakImagesTest < Minitest::Test
     end
   end
 
+  test "renders okay with no space after the opening tag" do
+    given_govspeak("^I am very informational") do
+      assert_html_output %(
+        <div role="note" aria-label="Information" class="application-notice info-notice">
+        <p>I am very informational</p>
+        </div>)
+      assert_text_output "I am very informational"
+    end
+  end
+
   test "avoids combining into a single block with an immediately-preceding line" do
     given_govspeak "The following is very informational\n^ I am very informational ^" do
       assert_html_output %(
@@ -33,6 +44,38 @@ class GovspeakImagesTest < Minitest::Test
         <p>I am very informational</p>
         </div>)
       assert_text_output "The following is very informational I am very informational"
+    end
+  end
+
+  describe "support for nested Kramdown" do
+    test "supports headings" do
+      given_govspeak "^ ## I am an informational heading" do
+        assert_html_output %(
+        <div role="note" aria-label="Information" class="application-notice info-notice">
+        <h2 id="i-am-an-informational-heading">I am an informational heading</h2>
+        </div>)
+        assert_text_output "I am an informational heading"
+      end
+    end
+
+    test "supports links" do
+      given_govspeak "^ [My informational link](https://www.gov.uk)" do
+        assert_html_output %(
+          <div role="note" aria-label="Information" class="application-notice info-notice">
+          <p><a href="https://www.gov.uk">My informational link</a></p>
+          </div>)
+        assert_text_output "My informational link"
+      end
+    end
+
+    test "supports bold emphasis" do
+      given_govspeak "^ I am **very** informational" do
+        assert_html_output %(
+          <div role="note" aria-label="Information" class="application-notice info-notice">
+          <p>I am <strong>very</strong> informational</p>
+          </div>)
+        assert_text_output "I am very informational"
+      end
     end
   end
 end
