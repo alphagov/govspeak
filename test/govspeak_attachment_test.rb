c4f067fdfc49f19cb32a9ef8ffb248c58ef22325
Ynda Jas <yndajas@gmail.com>
2025-08-14 12:11:46 +0100

    WIP mobbing AST attachement test

--- test/govspeak_attachment_test.rb
+++ test/govspeak_attachment_test.rb
@@ -1,12 +1,45 @@
 require "test_helper"
+require "govspeak_test_helper"
 
 class GovspeakAttachmentTest < Minitest::Test
+  include GovspeakTestHelper
+
   def render_govspeak(govspeak, attachments = [])
     Govspeak::Document.new(govspeak, attachments:).to_html
   end
 
   test "renders an empty string for attachment link that is not found" do
     assert_equal("\n", render_govspeak("[Attachment:file.pdf]", []))
+  end
+
+  test_given_govspeak(
+    "[Attachment:attachment.pdf]",
+    { attachments: [
+      {
+        id: "attachment.pdf",
+        url: "http://example.com/attachment.pdf",
+        title: "Attachment Title",
+      },
+    ] },
+  ) do
+    # assert_html_output %(
+    #   first
+    #   second
+    # )
+    assert_text_output "Attachment Title"
+    assert_ast_output_pattern do |actual|
+      actual => {
+        type: :root,
+        children: [
+          {
+            type: :embed_attachment,
+            id: "attachment.pdf",
+            url: "http://example.com/attachment.pdf",
+            title: "Attachment Title",
+          }
+        ]
+      }
+    end
   end
 
   test "renders an attachment component for a found attachment" do
