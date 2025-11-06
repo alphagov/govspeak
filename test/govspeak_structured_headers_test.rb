308a9a9ee4a25fa7a6cd2774ac9ccaf1c520e8c8
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-11-06 09:53:42 +0000

    Add auto-numbered headers to structured headers
    - Here we are working with an existing structure, and can use a recursive method
      to add the numbering. Note that this works down from level 2 as far as the structure
      goes, so will provide more auto-numbering than the html generator (which only
      numbers h2s and h3s at the moment, mirroring the behaviour in Whitehall)

--- test/govspeak_structured_headers_test.rb
+++ test/govspeak_structured_headers_test.rb
@@ -145,4 +145,25 @@ class GovspeakStructuredHeadersTest < Minitest::Test
   test "document with single h1 produces no headers" do
     assert_equal [], Govspeak::Document.new("# Heading\n").structured_headers
   end
+
+  test "auto-numbered headers are generated when the option is set on the document" do
+    doc = Govspeak::Document.new(document_body, auto_numbered_headers: true)
+
+    headers = doc.structured_headers
+
+    assert_equal "1. Heading 1", headers[0][:text]
+    assert_equal "2. Heading 2", headers[1][:text]
+    assert_equal "2.1 Sub heading 2.1", headers[1][:headers][0][:text]
+    assert_equal "2.2 Sub heading 2.2", headers[1][:headers][1][:text]
+    assert_equal "2.2.1 Sub sub heading 2.2.1", headers[1][:headers][1][:headers][0][:text]
+    assert_equal "2.3 Sub heading 2.3", headers[1][:headers][2][:text]
+    assert_equal "3. Heading 3", headers[2][:text]
+    assert_equal "4. Heading 4", headers[3][:text]
+    assert_equal "4.1 Sub heading 4.1", headers[3][:headers][0][:text]
+    assert_equal "4.1.1 Sub sub heading 4.1.1", headers[3][:headers][0][:headers][0][:text]
+    assert_equal "4.1.1.1 Sub sub sub heading 4.1.1.1", headers[3][:headers][0][:headers][0][:headers][0][:text]
+    assert_equal "4.2 Sub heading 4.2", headers[3][:headers][1][:text]
+    assert_equal "5. Heading 5", headers[4][:text]
+    assert_equal "5.1 Sub heading 5.1", headers[4][:headers][0][:text]
+  end
 end
